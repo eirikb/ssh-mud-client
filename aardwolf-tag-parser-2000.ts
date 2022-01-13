04a2cc4d3ee3bfec50f10aed8aee7b69d00c2836
Eirik Brandtzæg <eirikb@eirikb.no>
2022-01-13 19:03:56 +0100

    Testiplesti

--- aardwolf-tag-parser-2000.ts
+++ aardwolf-tag-parser-2000.ts
@@ -14,50 +14,56 @@ export class AardwolfTagParser2000 extends Transform {
 
   private buffer: number[] = [];
 
-  // private tagName: string = "";
-
   override _transform(
     data: Uint8Array,
     _: BufferEncoding,
     callback: TransformCallback
   ) {
-    data = data.filter((byte) => byte !== cr);
-
     const pad = this.buffer.length;
-    for (let i = 0; i < data.length; i++) {
-      const byte = data[i];
-      this.buffer.push(byte || 0);
-      if (byte === lf && this.tag === 3) {
-        this.tag = 4;
-      } else if (i === 0 && (byte === a || byte === b)) {
-        this.begin1 = i + pad;
-        this.tag = 2;
-      } else if (byte === lf) {
-        this.tag = 1;
-      } else if (this.tag === 1 && (byte === a || byte === b)) {
-        this.begin1 = i + pad;
-        this.tag = 2;
-      } else if (this.tag === 2 && (byte === a2 || byte === b2)) {
-        this.begin2 = i + pad;
-        this.tag = 3;
-      } else if (this.tag === 1) {
-        this.tag = 0;
-      }
+    let i = -1;
+    for (let byte of data) {
+      if (byte !== cr) {
+        i++;
+        this.buffer.push(byte || 0);
+        if (byte === lf && this.tag === 3) {
+          this.tag = 4;
+        } else if (i === 0 && (byte === a || byte === b)) {
+          this.begin1 = i + pad;
+          this.tag = 2;
+        } else if (byte === lf) {
+          this.tag = 1;
+        } else if (this.tag === 1 && (byte === a || byte === b)) {
+          this.begin1 = i + pad;
+          this.tag = 2;
+        } else if (this.tag === 2 && (byte === a2 || byte === b2)) {
+          this.begin2 = i + pad;
+          this.tag = 3;
+        } else if (this.tag === 1) {
+          this.tag = 0;
+        }
 
-      if (this.tag === 4) {
-        const t = Buffer.from(this.buffer)
-          .slice(this.begin1 + 1, this.begin2)
-          .toString();
-        const d = Buffer.from(this.buffer)
-          .slice(this.begin2 + 1, i + pad)
-          .toString();
-        this.emit("tag", { tag: t, data: d });
-        this.tag = 1;
+        if (this.tag === 4) {
+          const t = Buffer.from(this.buffer)
+            .slice(this.begin1 + 1, this.begin2)
+            .toString();
+          const d = Buffer.from(this.buffer)
+            .slice(this.begin2 + 1, i + pad)
+            .toString();
+          this.emit("tag", { tag: t, data: d });
+
+          this.buffer = this.buffer
+            .slice(0, this.begin1)
+            .concat(this.buffer.slice(i + pad + 1));
+          this.tag = 1;
+        }
       }
     }
 
-    if (this.tag === 0) {
-      this.emit("data", Buffer.from(this.buffer));
+    // If we end with single (tag 1) \n just skip the parsing
+    if (this.tag === 1) this.tag = 0;
+
+    if (this.tag === 0 && this.buffer.length > 0) {
+      this.emit("data", Buffer.from(this.buffer).toString());
       this.buffer = [];
     }
 
--- tests/aardwolftest.ts
+++ tests/aardwolftest.ts
@@ -1,5 +1,64 @@
-import * as test from "ava";
+import test from "ava";
 
-test("well", (t) => {
-  t.pass();
+import { AardwolfTagParser2000 } from "../aardwolf-tag-parser-2000";
+
+function run(input: string) {
+  const data: string[] = [];
+  const tags: { tag: string; data: string }[] = [];
+  const p = new AardwolfTagParser2000();
+  p.on("data", (d) => data.push(d));
+  p.on("tag", (t) => tags.push(t));
+  p.write(input);
+  return { data, tags };
+}
+
+test("Beginning of chunk", (t: any) => {
+  const res = run("{hello}world\n");
+  console.log(res.data);
+  t.is(res.data.length, 0);
+  t.deepEqual(res.tags, [
+    {
+      tag: "hello",
+      data: "world",
+    },
+  ]);
+});
+
+test("Beginning of chunk with data", (t: any) => {
+  const res = run("{hello}world\nwith data");
+  t.deepEqual(res.data, ["with data"]);
+  t.deepEqual(res.tags, [
+    {
+      tag: "hello",
+      data: "world",
+    },
+  ]);
+});
+
+test("Beginning of line", (t: any) => {
+  const res = run("with\n{hello}world\ndata");
+  t.deepEqual(res.data, ["with\ndata"]);
+  t.deepEqual(res.tags, [
+    {
+      tag: "hello",
+      data: "world",
+    },
+  ]);
+});
+
+test("NOT unless beginning of chunk or line", (t: any) => {
+  const res = run("with\ne{hello world}\n");
+  t.deepEqual(res.data, ["with\ne{hello world}\n"]);
+  t.is(res.tags.length, 0);
+});
+
+test("<> as well of chunk or line", (t: any) => {
+  const res = run("hello\n<hello>world\n");
+  t.deepEqual(res.data, ["hello\n"]);
+  t.deepEqual(res.tags, [
+    {
+      tag: "hello",
+      data: "world",
+    },
+  ]);
 });
