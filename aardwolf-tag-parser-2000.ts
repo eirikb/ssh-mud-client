402726bdab1b0de743f35be7b3d099a20c0895fe
Eirik Brandtzæg <eirikb@eirikb.no>
2022-01-13 20:12:32 +0100

    Tell me about it

--- aardwolf-tag-parser-2000.ts
+++ aardwolf-tag-parser-2000.ts
@@ -1,5 +1,34 @@
 import { Transform, TransformCallback } from "stream";
 
+/**
+ * Hi.
+ * What you see here might not look pretty, because it's not.
+ *
+ * Parsing output from a MUD turned out to be pretty complicated.
+ * At first, I tried parsing line by line.
+ * But it turns out MUDs often don't send a newline on prompt.
+ * And data is sent in chunks, meaning you might not see the full output yet.
+ * In other words it is impossible to know when the data has ended,
+ * without detecting the prompt, but that's hard since the prompt can be user defined.
+ *
+ * Notes on approaches:
+ *   * Aardwolf allows disabling prompt. This makes newline parsing possible,
+ *     but the prompt can include additional information, and it can't be queried over GMCP.
+ *   * Aardwolf allows setting a custom prompt, so one could force the prompt to end with a newline.
+ *     This could work, but there are different types of prompts, e.g., paging.
+ *     And I don't want to force this on users.
+ *
+ *  What I have done instead:
+ *    * Deal with the data as a stream of bytes, and spit that out to the user.
+ *    * Search for Aardwolf tags, such as <> and {} on this basis:
+ *      ** If { or < starts the chunk it is a tag. It could not be, as it could be in a chat,
+ *         but very unlikely.
+ *      ** If \n is followed by { or < it is a tag
+ *      ** If the tag has data between } or > and \n that is tag data.
+ *      ** If a tag has tag data, unless it is called "bigmap", it ends.
+ *      ** Tags without data don't end until their corresponding tag.
+ */
+
 const cr = "\r".charCodeAt(0);
 const lf = "\n".charCodeAt(0);
 const a = "<".charCodeAt(0);
@@ -20,11 +49,10 @@ export class AardwolfTagParser2000 extends Transform {
     callback: TransformCallback
   ) {
     const pad = this.buffer.length;
-    let i = -1;
     for (let byte of data) {
       if (byte !== cr) {
-        i++;
         this.buffer.push(byte || 0);
+        const i = this.buffer.length;
         if (byte === lf && this.tag === 3) {
           this.tag = 4;
         } else if (i === 0 && (byte === a || byte === b)) {
