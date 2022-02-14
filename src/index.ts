8ac11a0eb619b4314978b602c739c191f63ec3af
Eirik Brandtzæg <eirikb@eirikb.no>
2022-02-14 21:50:03 +0100

    Try-catch whole client
    `.on('error'` didn't seem to work very well

--- src/index.ts
+++ src/index.ts
@@ -16,80 +16,87 @@ new Server(
       sign: "",
       username: "",
     };
-    client
-      .on("authentication", (ctx) => {
-        console.log(ctx.username, ctx.method);
+    try {
+      client
+        .on("authentication", (ctx) => {
+          console.log(ctx.username, ctx.method);
 
-        if (ctx.method === "none") return ctx.reject();
-        else if (ctx.method === "keyboard-interactive") ctx.accept();
-        else if (ctx.method === "password") return ctx.reject();
-
-        userInfo.username = ctx.username;
-        userInfo.lastLogin = lastLogins[userInfo.username] || 0;
-        lastLogins[userInfo.username] = Date.now();
-
-        const k = (ctx as any).key;
-        if (k) {
-          userInfo.key = k.data.toString("hex");
-        }
-        userInfo.sign = (ctx as any).sign;
-
-        console.log("auth", userInfo);
-        ctx.accept();
-      })
-      .on("session", (accept) => {
-        const session = accept();
-        let rows = 0;
-        let cols = 0;
-
-        session.on("pty", (accept, _, info) => {
-          console.log("pty", userInfo.username, info);
-          rows = info.rows;
-          cols = info.cols;
-          if (accept) accept();
-        });
-
-        session.on("shell", (accept) => {
-          console.log("shell", userInfo.username);
-          const stream = accept();
-
-          const screen = blessed.screen({
-            smartCSR: true,
-            terminal: "xterm-256color",
-            fullUnicode: true,
-            input: stream,
-            output: stream,
-          });
-
-          function resize() {
-            // @ts-ignore
-            stream.rows = rows;
-            // @ts-ignore
-            stream.columns = cols;
-            stream.emit("resize");
+          if (ctx.method === "none") return ctx.reject();
+          else if (ctx.method === "keyboard-interactive") ctx.accept();
+          else if (ctx.method === "password") {
+            console.log(ctx.password);
+            return ctx.reject();
           }
 
-          session.on("window-change", (_, __, info) => {
-            console.log("resize", info);
+          userInfo.username = ctx.username;
+          userInfo.lastLogin = lastLogins[userInfo.username] || 0;
+          lastLogins[userInfo.username] = Date.now();
+
+          const k = (ctx as any).key;
+          if (k) {
+            userInfo.key = k.data.toString("hex");
+          }
+          userInfo.sign = (ctx as any).sign;
+
+          console.log("auth", userInfo);
+          ctx.accept();
+        })
+        .on("session", (accept) => {
+          const session = accept();
+          let rows = 0;
+          let cols = 0;
+
+          session.on("pty", (accept, _, info) => {
+            console.log("pty", userInfo.username, info);
             rows = info.rows;
             cols = info.cols;
+            if (accept) accept();
+          });
+
+          session.on("shell", (accept) => {
+            console.log("shell", userInfo.username);
+            const stream = accept();
+
+            const screen = blessed.screen({
+              smartCSR: true,
+              terminal: "xterm-256color",
+              fullUnicode: true,
+              input: stream,
+              output: stream,
+            });
+
+            function resize() {
+              // @ts-ignore
+              stream.rows = rows;
+              // @ts-ignore
+              stream.columns = cols;
+              stream.emit("resize");
+            }
+
+            session.on("window-change", (_, __, info) => {
+              console.log("resize", info);
+              rows = info.rows;
+              cols = info.cols;
+              resize();
+            });
+
             resize();
-          });
 
-          resize();
+            screen.on("destroy", () => {
+              stream.exit(0);
+              stream.end();
+            });
+            client.on("close", () => {
+              console.log(userInfo.username, "close");
+              screen.destroy();
+            });
 
-          screen.on("destroy", () => {
-            stream.exit(0);
-            stream.end();
+            lobby(screen, userInfo);
           });
-          client.on("close", () => {
-            console.log(userInfo.username, "close");
-            screen.destroy();
-          });
-
-          lobby(screen, userInfo);
         });
-      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 )
   .on("error", (err) => {
